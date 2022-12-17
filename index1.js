a31de10b04fdeb66cb730e770df911f4b1e28649
md nabab sharif <mdnababsharifdiu@gmail.com>
2022-12-17 09:15:59 +0600

    Reducers

---
+++ index1.js
@@ -0,0 +1,57 @@
+
+//defining constants
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+
+//state
+const initialCounterState = {
+  count: 0,
+};
+
+
+//action-Object-type, payload
+
+const incrementCounter = () => {
+  return {
+    type: INCREMENT,
+  }
+}
+
+const decrementCounter = () => {
+  return {
+    type: DECREMENT,
+  }
+}
+
+//create reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+
+  switch (action.type) {
+    case INCREMENT:
+
+       return {
+        ...state,
+        count:state.count+1
+       }
+
+    case DECREMENT:
+
+       return{
+        ...state,
+        count:state.count-1,
+       }
+
+
+    default:
+      state;
+
+  }
+}
+
+
+
+//1.state
+//2.dispatch action
+//3.reducer
+//4.store
