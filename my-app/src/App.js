c6e8ec59de7594a71f2ec4b65717f0fd4ccbe2f4
md nabab sharif <mdnababsharifdiu@gmail.com>
2022-12-20 10:25:26 +0600

    complete counter react-redux app

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Counter from './component/Counter2'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome to react-redux </h1>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
